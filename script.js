9a9ae71a6e21bc6f47a82833a54a47ddabeef314
surendra019 <145987917+surendra019@users.noreply.github.com>
2024-03-13 15:05:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 const toastContainer = document.getElementById('liveToastContainer');
+let books_showing = false;
 
 function showToast(msg, head = "Information") {
     let code = `<div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
@@ -37,11 +38,18 @@ function add_entry() {
             "category": category
         }
         fetch("http://localhost:3000/add_entry", { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) }).then(res => {
-            console.log(res);
+            if (res.status === 201) {
+                showToast("The book: " + book_name + " has been addeed successfully");
+                book_title_input.value = "";
+                book_author_input.value = "";
+                category_input.value = "";
+            } else if (res.status === 500) {
+                showToast("Error", "An error has occured while adding the book!");
+            }
         })
     } else {
 
-        showToast("Please fill all the fields");
+        showToast("Please fill all the fields!", "Warning");
     }
 
 
@@ -58,19 +66,72 @@ function remove_entry() {
             "author_name": author_name
         }
         fetch("http://localhost:3000/remove_entry", { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) }).then(res => {
-            return res.text();
-        }).then(res =>{
-            showToast("No such book present");
+            if (res.status === 201) {
+                showToast("The book: " + book_name + " has been removed successfully");
+                book_title_input.value = ""
+                book_author_input.value = ""
+
+            } else if (res.status === 500) {
+                showToast("Error", "An error has occured while removing the book!");
+            }
         })
-    }else{
+    } else {
         showToast("Please enter all the fields");
     }
 
 
 }
 
-function show_books(){
-    fetch("http://localhost:/3000/show_books", (res)=>{
-        
+function show_books() {
+    let books_container = document.getElementById("books_show_table");
+    let show_books_button = document.getElementById("show_books_button");
+    if (!books_showing) {
+        fetch("http://localhost:3000/show_books").then(res => {
+            if (res.status === 200) {
+                return res.json();
+            }
+            return new Promise((resolve, reject) => {
+                reject("an Error has been encountered!");
+            })
+        }).then((res) => {
+            books_container.innerHTML = `<tr>
+        <th>Book Name</th>
+        <th>Author</th>
+        <th>Category</th>
+    </tr>`;
+            for (key in res) {
+                let book_name = res[key]["book_name"];
+                let author_name = res[key]["author"];
+                let category = res[key]["category"];
+                let tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.innerHTML = book_name;
+                tr.appendChild(td);
+                var td2 = document.createElement("td");
+                td2.innerHTML = author_name;
+                tr.appendChild(td2);
+                var td3 = document.createElement("td");
+                td3.innerHTML = category;
+                tr.appendChild(td3);
+                books_container.appendChild(tr);
+            }
+            books_showing = true;
+            show_books_button.innerHTML = "Hide books";
+        })
+    } else {
+        books_container.innerHTML = "";
+        show_books_button.innerHTML = "Show books";
+        books_showing = false;
+    }
+
+}
+
+function check_connection() {
+    fetch("http://localhost:3000/check_connection").then((res) => {
+        if (res.status === 200) {
+            showToast("Connected to the database!");
+        } else {
+            showToast("Not connected to the database!");
+        }
     })
 }
--- server.js
+++ server.js
@@ -3,44 +3,154 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
+let database;
 
-var con = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "root",
-    database: "library"
-});
+
+
+
+//middlewares
+
 app.use(bodyParser.json());
 app.use(cors());
 
+//creating a connection to the server
+var con = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "password",
+});
+
+// checks if the connection is established
+con.connect((err) => {
+    if (err) console.error(err);
+    else {
+        console.log("Connection to the server is successful");
+    }
+})
+
+
+
+// create a 'library' database if there isn't exists one
+function create_database_if_not_exists(database_name) {
+    return new Promise((resolve, reject) => {
+        con.query(`create database if not exists ${database_name}`, (err, result) => {
+            if (err) console.log(err);
+            if (result.warningCount === 0) {
+                console.log("Database created successfully");
+
+            } else {
+                console.log("The database already exists");
+            }
+            con.end();
+
+            database = 'library';
+            con = mysql.createConnection({
+                host: "127.0.0.1",
+                user: "root",
+                password: "password",
+                database: "library"
+            })
+            con.connect((err) => {
+                if (err) console.log(err);
+                else {
+                    console.log(`successfully connected to the database ${database}`)
+                    resolve();
+                }
+            })
+
+        });
+    })
+}
+
+
+
+// create the required tables if they won't exist in the library database
+async function create_table_if_not_exists(table_name) {
+
+    con.query(`CREATE TABLE ${table_name}(id int auto_increment key, book_name varchar(20), author varchar(15), category varchar(10))`, (err, result) => {
+
+        if (err) console.log(err.sqlMessage);
+        else {
+            console.log("The books table is successfully created");
+        }
+
+
+    })
+}
+
+async function create_table_if_not_exists(table_name) {
+
+    con.query(`CREATE TABLE ${table_name}(id int auto_increment key, book_name varchar(20), author varchar(15), category varchar(10))`, (err, result) => {
+
+        if (err) console.log(err.sqlMessage);
+        else {
+            console.log("The books table is successfully created");
+        }
+
+
+    })
+}
+
+async function create_tables() {
+    await create_database_if_not_exists("library");
+    create_table_if_not_exists("books");
+
+}
+
+create_tables();
+
+
+
+// Routing
+
+app.get('/check_connection', (req, res) => {
+    res.status(200).send("Connected to the database successfully!")
+})
+
+// a post request to add a book into the 'books' table in the database 'library'
 app.post('/add_entry', (req, res) => {
     let sql = `insert into books(book_name, author, category) values("${req.body.book_name}", "${req.body.author_name}", "${req.body.category}")`;
 
     con.query(sql, (err, result) => {
-        if (err) throw err;
-        else {
-            console.log(result);
+        if (err) {
+            console.log(err.sqlMessage)
+            res.status(500).send("Error adding a book : " + err.sqlMessage);
+            return;
         }
 
     })
-    res.status(201).send("Success");
+    res.status(201).send("Successfully added the book: "+ req.body.book_name);
 })
 
+// remove a book from the 'books' table in the database 'library'
 app.post('/remove_entry', async (req, res) => {
     let sql = `delete from books where book_name = "${req.body.book_name}" and author = "${req.body.author_name}"`;
 
     // let sql = "delete from books where id = 1";
     con.query(sql, (err, result) => {
-        if (err) throw err;
-        else {
-            if(result.affectedRows==0){
-                res.status(200).send("No such book found");
-            }
+        if (err) {
+            res.status(500).send("Error removing a book: "+ err.sqlMessage);
+            return;
         }
+        res.status(201).send("Successfully removed the book: "+ req.body.book_name);
+        
     })
 
 })
 
+app.get('/show_books', (req, res) =>{
+    let sql = `select * from books`;
+    con.query(sql, (err, result) => {
+        if (err) {
+            res.status(500).send("An unexpected error occurred: "+ err.sqlMessage);
+        }else{
+            res.status(200).send(result);
+        }
+    })
+})
+
+
+// hoisting the nodejs server locally over a port
 const PORT = 3000; // Choose any port you like
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
